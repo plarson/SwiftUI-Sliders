7a07bc72c4e791f8d96f1f8ab7792ed923c3c3b2
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-11-18 09:25:12 -0500

    Modify the package dependency

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sliders",
+    name: "Sliders-SwiftUI",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Sliders",
-            targets: ["Sliders"]),
+            name: "Sliders-SwiftUI",
+            targets: ["Sliders-SwiftUI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/tsaihong1995/CGExtender.git", from: "1.0.1"),
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Sliders",
+            name: "Sliders-SwiftUI",
             dependencies: ["CGExtender", "Shapes", "bez"]),
         .testTarget(
             name: "SlidersTests",
-            dependencies: ["Sliders"]),
+            dependencies: ["Sliders-SwiftUI"]),
     ]
 )
